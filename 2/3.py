cf15a7a4a9eca2b5130dfdb27f1c41768cfa3677
Sashok <sergeevbass@gmail.com>
2022-12-21 08:51:44 +0500

    Resolve second HW

--- 2/3.py
+++ 2/3.py
@@ -2,3 +2,22 @@
 # Найдите произведение элементов на указанных ИНДЕКСАХ. 
 # Пять ИНДЕКСОВ хранятся в списке, который вы сами заполняете.
 # Пример списка ИНДЕКСОВ [2, 2, 3, 1, 8]
+
+
+length = int(input('Input length of the array: '))
+lst1 = []
+for i in range(-length, length + 1):
+    lst1.append(i)
+print(lst1)
+
+lst2 = [1,4,5,2,3]
+multi = 1
+for i in lst2:
+    multi *= lst1[i]
+print(multi)
+
+
+
+
+
+
